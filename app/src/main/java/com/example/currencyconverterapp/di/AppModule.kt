71d5180bf1907f13568c75869cdb2ea233e1de4a
Joaquín Alaguive <joacoalaguive@gmail.com>
2021-03-03 23:10:32 -0300

    Advances on the Module

--- app/src/main/java/com/example/currencyconverterapp/di/AppModule.kt
+++ app/src/main/java/com/example/currencyconverterapp/di/AppModule.kt
@@ -1,7 +1,50 @@
 package com.example.currencyconverterapp.di
 
+import com.example.currencyconverterapp.data.CurrencyApi
+import com.example.currencyconverterapp.main.DefaultMainRepository
+import com.example.currencyconverterapp.main.MainRepository
+import com.example.currencyconverterapp.util.DispatcherProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://api.exchangeratesapi.io/"
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
+    // Solo tenemos una instancia de esto en nuestra app, no sera repetida en ningun otro sitio
+    @Singleton
+    @Provides
+    fun provideCurrencyApi(): CurrencyApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(CurrencyApi::class.java)
 
+    @Singleton
+    @Provides
+    fun providesMainRepository(api: CurrencyApi) : MainRepository = DefaultMainRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideDispatchers() : DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
 
 }
---
+++ app/src/main/java/com/example/currencyconverterapp/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.currencyconverterapp.util
+
+object Constants {
+
+}
